68aab94176a642809e9b830fff6c295d06738ef7
panditiavinash <=>
2023-06-22 17:53:18 +1000

    Added Page Title Validations

--- web/cypress/e2e/festivals.cy.ts
+++ web/cypress/e2e/festivals.cy.ts
@@ -5,6 +5,7 @@ describe('EA Coding Test', () => {
   const festivalHelpers = new FestivalHelpers();
   it('Festivals Web Page Validations', () => {
     cy.visit('http://localhost:4200/festivals') // open a web page
+    cy.title().should('eq', 'EaCodingTest');
     festivalHelpers.festivalsEA('//ol/li') // validates the data inside the web page
   })
 })
